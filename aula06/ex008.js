ea4811a3a62423376d384ecdfe037497d12a0de4
Marco Antonio <marcoa.amaraldasilva@gmail.com>
2024-11-23 23:35:26 -0300

    novos exercícios

--- aula06/ex008.js
+++ aula06/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou!')
+var vel = 50;
+console.log(`A velocida do seu carro é de ${vel}`);
+if(vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. Multado!');
+}
+console.log('Dirija sempre usando sinto de segurança');
